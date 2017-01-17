cdbf32b9d7e54161714c11b37fcb5ab89c9a4675
Mark Paolo Libunao <markpaololibunao@gmail.com>
2017-01-18 00:06:38 +0800

    Fixed javascript file where jQuery wasn't selecting properly. Damn :

--- js/tribute.js
+++ js/tribute.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-  $("ul .unordered-trivia").children().addClass("trivia-text");
-  $('.trivia-text').css("font-size:", '10px')
+  $('ul').children().addClass('trivia-text');
+  $('.trivia-text').css("font-size", '10px');
 });
